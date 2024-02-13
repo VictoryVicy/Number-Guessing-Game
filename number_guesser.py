2e9c782c4ae06c6ac784268f02a50c43cb333cc4
VictoryVicy <obed.noni99@gmail.com>
2024-02-13 23:30:56 +0700

    final fix code, maybe?

--- number_guesser.py
+++ number_guesser.py
@@ -8,14 +8,15 @@ if top_of_range.isdigit():
     if top_of_range <= 0:
         print("Please type a number larger than 0 next time.")
         quit()
+else:
+    print("Please type a  number")
+    quit()
 
-    else:
-        print("Please type a  number")
-        quit()
-
-random_number = random.randint(0,top_of_range)
+random_number = random.randint(0, top_of_range)
+guesses = 0
 
 while True:
+    guesses += 1
     user_guess = input("Make a guess: ")
     if user_guess.isdigit():
         user_guess = int(user_guess)
@@ -25,5 +26,10 @@ while True:
 
     if user_guess == random_number:
         print("You got it!")
+        break
+    elif user_guess > random_number:
+        print("You were above the number!")
     else:
-        print("A A, you wrong wleee")
+        print("You were below the number!")
+
+print("You got it in", guesses, "guesses")
