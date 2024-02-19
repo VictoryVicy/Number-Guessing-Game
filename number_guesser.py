35b23b5365c0a3bb9d1a22db4e8d40be6ac30fa0
VictoryVicy <obed.noni99@gmail.com>
2024-02-19 23:44:48 +0700

    oke

--- number_guesser.py
+++ number_guesser.py
@@ -30,7 +30,6 @@ def game():
     top_of_range = 100
     random_number = random.randint(0, top_of_range)
     guesses = 0
-    
 
     # Loop permainan
     running = True
@@ -42,35 +41,31 @@ def game():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
-
-        # Handling input tebakan pengguna
-        key = pygame.key.get_pressed()
-        if key[pygame.K_UP]:
-            guesses += 1
-            user_guess = random.randint(0, top_of_range)
-            if user_guess == random_number:
-                print("Selamat, kamu benar!")
-                running = False
-            elif user_guess > random_number:
-                print("Masih di atas nomor yang benar nih, turunkan tebakanmu..")
-            else:
-                print("Kalau ini, di bawah nomor yang benar, naikkan tebakanmu..")
-        elif key[pygame.K_DOWN]:
-            guesses -= 1
-            user_guess = random.randint(0, top_of_range)
-            if user_guess == random_number:
-                print("Selamat, kamu benar!")
-                running = False
-            elif user_guess > random_number:
-                print("Masih di atas nomor yang benar nih, turunkan tebakanmu..")
-            else:
-                print("Kalau ini, di bawah nomor yang benar, naikkan tebakanmu..")
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if plus_button_rect.collidepoint(event.pos):
+                    guesses += 1
+                elif minus_button_rect.collidepoint(event.pos):
+                    guesses -= 1
+                elif check_button_rect.collidepoint(event.pos):
+                    if guesses == random_number:
+                        print("Selamat, kamu benar!")
+                        running = False
+                    elif guesses > random_number:
+                        print("Masih di atas nomor yang benar nih, turunkan tebakanmu..")
+                    else:
+                        print("Kalau ini, di bawah nomor yang benar, naikkan tebakanmu..")
 
         # Tampilan permainan
         screen.fill(WHITE)
         draw_text(screen, "Tebak Angka", font, BLACK, SCREEN_WIDTH // 2, 50)
         draw_text(screen, "Tebakan: {}".format(guesses), font, BLACK, SCREEN_WIDTH // 2, 100)
         draw_text(screen, "Tekan panah atas untuk menebak", font, BLACK, SCREEN_WIDTH // 2, 150)
+        pygame.draw.rect(screen, GRAY, plus_button_rect)
+        pygame.draw.rect(screen, GRAY, minus_button_rect)
+        pygame.draw.rect(screen, GRAY, check_button_rect)
+        draw_text(screen, "+", font, BLACK, plus_button_rect.centerx, plus_button_rect.centery)
+        draw_text(screen, "-", font, BLACK, minus_button_rect.centerx, minus_button_rect.centery)
+        draw_text(screen, "Cek", font, BLACK, check_button_rect.centerx, check_button_rect.centery)
         pygame.display.flip()
 
 # Main loop
@@ -82,6 +77,15 @@ start_button_rect = pygame.Rect(SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 - 30
 
 # Tombol "Keluar"
 quit_button_rect = pygame.Rect(SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 + 50, 200, 50)
+
+# Tombol +
+plus_button_rect = pygame.Rect(SCREEN_WIDTH // 2 - 70, SCREEN_HEIGHT // 2 + 100, 50, 50)
+
+# Tombol -
+minus_button_rect = pygame.Rect(SCREEN_WIDTH // 2 + 20, SCREEN_HEIGHT // 2 + 100, 50, 50)
+
+# Tombol Cek
+check_button_rect = pygame.Rect(SCREEN_WIDTH // 2 - 25, SCREEN_HEIGHT // 2 + 180, 100, 50)
 
 while True:
     screen.fill(WHITE)
